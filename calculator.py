f6950c1b1d5cf2e8bbb913c3d769f0f6c32cce31
Jerry Chen <jchen056@citymail.cuny.edu>
2022-10-27 13:23:54 -0400

    calculator

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+from scipy.stats import binom
+
+st.header("Binomial Calculator")
+st.write('X~Binomial(n,p)')
+st.write('The possible values of X range from 0 to n and the probabilities for each of those values are given by')
+st.latex(r'''P(X=x)={n \choose x} p^x (1-p)^{n-x}''')
+
+num3=st.number_input(label='Insert a number for the number of trials',min_value=1)
+num4=st.number_input(label='Select a number for p',
+min_value=0.0,
+max_value=1.0,value=0.5,step=0.01)
+
+num1=st.number_input(label='Enter a number for the number of successes (x)',min_value=0,max_value=num3)
+st.write("Number of successes (x)",num1)
+
+pmfx=binom.pmf(num1,num3,num4)
+st.write("Binomial probability P(X=x):",pmfx)
+cdfx= binom.cdf(num1, num3, num4)
+st.write("Cumulative probability P(X<=x):",cdfx)
