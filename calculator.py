1340617bf55b2176d6aed0d88f1346294083ff65
Jerry Chen <jchen056@citymail.cuny.edu>
2022-10-27 13:38:35 -0400

    new cal large samples

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from scipy.stats import binom
+# from scipy.stats import binom
 
 st.header("Binomial Calculator")
 st.write('X~Binomial(n,p)')
@@ -15,7 +15,11 @@ max_value=1.0,value=0.5,step=0.01)
 num1=st.number_input(label='Enter a number for the number of successes (x)',min_value=0,max_value=num3)
 st.write("Number of successes (x)",num1)
 
-pmfx=binom.pmf(num1,num3,num4)
+# pmfx=binom.pmf(num1,num3,num4)
+# st.write("Binomial probability P(X=x):",pmfx)
+# cdfx= binom.cdf(num1, num3, num4)
+# st.write("Cumulative probability P(X<=x):",cdfx)
+
+#Let us try to estimate our PDF and PMF by drawing large samples
+pmfx=sum(np.random.binomial(num3,num4,100000)==num1)/100000
 st.write("Binomial probability P(X=x):",pmfx)
-cdfx= binom.cdf(num1, num3, num4)
-st.write("Cumulative probability P(X<=x):",cdfx)
